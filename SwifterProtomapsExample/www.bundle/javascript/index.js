feb5076bee573dab21d639226c5734d4c27f0408
sfomuseumbot <sfomuseumbot@localhost>
2024-12-30 11:25:24 -0800

    snapshot: wrangling, not sure why I can't zoom past z16...

--- SwifterProtomapsExample/www.bundle/javascript/index.js
+++ SwifterProtomapsExample/www.bundle/javascript/index.js
@@ -1,25 +1,24 @@
+var map_el = document.getElementById("map");
+map_el.innerText = "Loading map";
+
 var xhr = new XMLHttpRequest();
 xhr.open('HEAD', "http://localhost:9001/");
 xhr.onreadystatechange = function() {
     if (this.readyState == this.DONE) {
-        console.log("HELLO WORLD", this.status);
+        
+        map_el.innerHTML = "";
+        const map = L.map('map');
+            
+        const tile_url = "http://localhost:9001/pmtiles/sfo_v3.pmtiles";
+        const tile_theme = "white";
+        const tile_bounds = [ [37.601617, -122.408061], [37.640167, -122.354907] ];
+        const tile_layer = protomapsL.leafletLayer({url: tile_url, theme: tile_theme});
+            
+        tile_layer.addTo(map);
+        map.fitBounds(tile_bounds);
     }
 };
 
 xhr.send();
 
-const map = L.map('map');
-    
-const tile_url = "http://localhost:9001/pmtiles/sfo_v3.pmtiles";
-const tile_theme = "white";
-const tile_bounds = [ [37.601617, -122.408061], [37.640167, -122.354907] ];
-const tile_layer = protomapsL.leafletLayer({url: tile_url, theme: tile_theme});
-    
-tile_layer.addTo(map);
-map.fitBounds(tile_bounds);
 
-/*
-map.on("moveend", function(e){
-    console.log("MOVE", map.getZoom());
-});
-*/
